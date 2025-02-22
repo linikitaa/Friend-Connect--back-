607e36c804c38a38b4b4c0ced1d5025ef6da170d
linikitaa <linikitaa@yandex.ru>
2025-02-22 17:38:52 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,64 @@
+using System.Text;
+using Controllers;
+using Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString, x => x.MigrationsAssembly("FriendConnect"))
+);
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(UsersController).Assembly);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+builder.Services.AddAuthorization();
+// Настраиваем аутентификацию с JWT
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5101); // HTTP
+    options.ListenAnyIP(7085, listenOptions =>
+    {
+        listenOptions.UseHttps(); // Включаем HTTPS
+    });
+});
+var app = builder.Build();
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+app.UseCors("AllowReactApp");
+app.UseHttpsRedirection();
+app.UseAuthentication(); 
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
